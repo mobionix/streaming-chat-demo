e3ff4f73b22ccd176d473d5993a0b4efc4daf8dc
jegabyte <110680381+jegabyte@users.noreply.github.com>
2023-08-18 23:10:21 +0800

    Update config.js

--- web-ui/src/config.js
+++ web-ui/src/config.js
@@ -5,15 +5,15 @@
 
 // Amazon IVS Playback URL
 // Replace this with your own Amazon IVS Playback URL
-export const PLAYBACK_URL = "https://760b256a3da8.us-east-1.playback.live-video.net/api/video/v1/us-east-1.049054135175.channel.6tM2Z9kY16nH.m3u8";
+export const PLAYBACK_URL = "https://f3e97143f6f2.ap-south-1.playback.live-video.net/api/video/v1/ap-south-1.322748982444.channel.UVjbceswmyPv.m3u8";
 
 // Chat websocket address
 // The AWS region that your room is created in. For example, `us-west-2`.
-export const CHAT_REGION = "";
+export const CHAT_REGION = "ap-south-1";
 
 // Chat API URL
 // The Amazon IVS Chat backend endpoint. You must deploy the serverless backend to get this value.
-export const API_URL = "";
+export const API_URL = "https://u74w8ix1k8.execute-api.ap-south-1.amazonaws.com/Prod/";
 
 // Chat room id (ARN)
-export const CHAT_ROOM_ID = "";
+export const CHAT_ROOM_ID = "arn:aws:ivschat:ap-south-1:322748982444:room/iNkpKUhZ5hQ3";
